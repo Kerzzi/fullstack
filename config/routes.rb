58a79625fb5ada365af1a2a335149498eb7bfb9c
Kerzzi <kerzzi@outlook.com>
2017-11-09 21:02:05 +0800

    增加后台路由

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,11 @@ Rails.application.routes.draw do
   
   namespace :admin do
     resources :posts
-    resources :courses
+    resources :courses do
+      resources :syllabuses
+      resources :assignments
+      resources :faqs
+      resources :activities
+    end
   end
 end
